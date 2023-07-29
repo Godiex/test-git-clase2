3772571cd365f5759a6d07d6cdbc5638c1577206
Anderson_Garcia <anderson.garcia@school.devzeros.com>
2023-07-29 10:43:51 -0500

    atributo clase empleado

--- AppConsola/Empleado.cs
+++ AppConsola/Empleado.cs
@@ -2,5 +2,18 @@ namespace AppConsola;
 
 public class Empleado
 {
+    public string Id {get; set;} 
     public string Nombre { get; set; } = string.Empty;
+    public string Cargo { get; set;} 
+    public int Edad {get; set;}
+    public int DepartamentoId {get; set;}
+
+    public Empleado(string nombre,string cargo,int edad,int departamentoId)
+    {
+        Nombre = nombre;
+        Cargo = cargo;
+        Edad = edad;
+        DepartamentoId = departamentoId;
+    }
 }
+ 
