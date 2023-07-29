c65134f1b90df8c7b8a25f43760e8d54afdcb8e6
Eduardo_Zequeira <eduardozequeira0@gmail.com>
2023-07-29 13:06:06 -0500

    cambios base de datos

--- AppConsola/CadenaConexion.cs
+++ AppConsola/CadenaConexion.cs
@@ -2,5 +2,5 @@ namespace AppConsola;
 
 public static class CadenaConexion
 {
-    public const string VALOR = "Server=localhost;Database=MiEmpresa;Trusted_Connection=True;Encrypt=False";
+    public const string VALOR = "Server=localhost;Database=MIEmpresa;Trusted_Connection=True;Encrypt=False";
 }
