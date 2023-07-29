b1cb90d94724da1208374483570d14eda5be9654
Anderson_Garcia <anderson.garcia@school.devzeros.com>
2023-07-29 09:57:48 -0500

    einer danilo

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Probando git");
+Console.WriteLine("Mi nombre es einer danilo");
+
