82957322e35a9d05d7c3f95c4e2d518b044896e5
diego.villa <dvillcar@contratista.epm.co>
2023-07-29 09:55:01 -0500

    ajuste el program

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Probando git");
+Console.WriteLine("mi nombre es diego villa");
